310a99e0fe32b70e4c0c98503e9588c36c2fd25c
xakash00 <79003567+xakash00@users.noreply.github.com>
2021-08-22 20:18:26 +0530

    updated about page

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import img1 from "../images/goku.png";
 import Common from "../Component/Common";
 
-const About = () => {
+const About = (props) => {
   return (
     <>
       <div className="aboutPage">
         <Common
+          mode={props.mode}
           name="About"
           imgsrc={img1}
           visit="/contact"
